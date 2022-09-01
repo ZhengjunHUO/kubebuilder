f0be70da5173256901acb9f1004670cf981a41d6
ZhengjunHUO <firelouiszj@hotmail.com>
2022-09-01 08:02:55 +0200

    add permission in clusterrole on event

--- controllers/fufu_controller.go
+++ controllers/fufu_controller.go
@@ -43,6 +43,7 @@ type FufuReconciler struct {
 //+kubebuilder:rbac:groups=cat.huozj.io,resources=fufus,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=cat.huozj.io,resources=fufus/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cat.huozj.io,resources=fufus/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
